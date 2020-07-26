5bb6e47423c1679e8b6c64e1d5d7d4c484440e84
Ricky Chon <rickychon99@gmail.com>
2020-07-26 17:04:32 -0400

    Initial commit

---
+++ src/main/java/io/github/rickyc0626/skilltree/SkillTree.java
@@ -0,0 +1,18 @@
+package io.github.rickyc0626.skilltree;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkillTree extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
